a300a17b8f3f7659585c49f8d249f3f526a2633a
Kaveripansare <109793018+Kaveripansare@users.noreply.github.com>
2022-09-08 13:02:15 +0530

    Add files via upload

---
+++ Lab Test 1/src/OddNumber.java
@@ -0,0 +1,16 @@
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		System.out.println("odd number between 1 to 20");
+     for(i=0;i<=20;i++) {
+    	 if(i%2!=0) {
+    		 System.out.println(i);
+    	 }
+    	 
+     }
+	}
+
+}
