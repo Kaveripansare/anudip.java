6cbf89e7cf486d7da9d42be94ea36da70775b071
Kaveripansare <109793018+Kaveripansare@users.noreply.github.com>
2022-09-29 14:55:02 +0530

    Add files via upload

---
+++ Lab Test 4/src/Area/Shape.java
@@ -0,0 +1,9 @@
+package Area;
+
+public interface Shape {//interface shape
+
+	//abstract method
+	double RectangleArea(double length,double bredth);
+	double SquareArea(double length);
+	double CircleArea(double radius);
+}
---
+++ Lab Test 4/src/Bank/Bank_C.java
@@ -0,0 +1,12 @@
+package Bank;
+
+public class Bank_C extends Bank{
+
+	@Override
+	void getBalance(int balance) {
+		// TODO Auto-generated method stub
+		System.out.println("the Rs "+balance+" deposited by you in a Bank_C.");
+	}
+
+}
+
